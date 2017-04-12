2e5d1e662973392151a62446a6aa567c4445dc15
kickscar <kickscar@kickscar-pc>
2017-04-13 00:53:53 +0900

    1st commit

---
+++ src/prob4/Prob4.java
@@ -0,0 +1,24 @@
+package prob4;
+
+import java.util.Scanner;
+
+public class Prob4 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String s = scanner.nextLine();
+		
+		char[] c = reverse( s );
+		printCharArray( c );
+		
+		scanner.close();
+	}
+	
+	public static char[] reverse(String str) {
+		/* 코드를 완성합니다 */
+	}
+
+	public static void printCharArray(char[] array){
+		/* 코드를 완성합니다 */
+	}
+}
